ae5927214460c3cf9abc1339dfb52fed012314af
Leona Araujo <araujo.contato@gmail.com>
2018-10-23 23:43:37 -0300

    Diplomata Inspector fixes removed serialized talkable on edit mode from scene

--- Diplomata/DiplomataData.cs
+++ Diplomata/DiplomataData.cs
@@ -24,12 +24,6 @@ namespace LavaLeak.Diplomata
     public DiplomataEventController EventController = new DiplomataEventController();
     public bool OnATalk;
 
-    private void Awake()
-    {
-      Reset();
-      DontDestroyOnLoad(gameObject);
-    }
-
     /// <summary>
     /// Reset all fields.
     /// </summary>
